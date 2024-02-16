a7d4377ba888aab8e5f12f5ae7b60b088bd20fb9
The-BB <tun.chen.bo@gmail.com>
2024-02-17 01:29:59 +0300

    backup-recover.sh: update (sync with OpenWrt, 2024.02)

--- backup-recover.sh
+++ backup-recover.sh
@@ -15,7 +15,6 @@ $PATCH_DIR/package-utils-e2fsprogs-add-libe2p.patch
 PATCH_PACKAGES="\
 $PATCH_DIR/libs-libmpeg2-add-as-dependency.patch
 $PATCH_DIR/libs-libvpx-add-as-dependency.patch
-$PATCH_DIR/libs-nss-add-patch.patch
 $PATCH_DIR/multimedia-ffmpeg-disable-sndio.patch
 $PATCH_DIR/multimedia-gst1-libav-add-as-dependency.patch
 $PATCH_DIR/multimedia-gst1-plugins-base-add-as-dependency.patch
@@ -30,15 +29,14 @@ STAMP_PACKAGE="$PATCH_DIR/.package-patched"
 STAMP_PACKAGES="$PATCH_DIR/.packages-patched"
 
 GO_PKG="\
+$PACKAGES_FEED/net/alist
 $PACKAGES_FEED/net/cloudreve
+$PACKAGES_FEED/net/croc
 $PACKAGES_FEED/net/dnsproxy
 $PACKAGES_FEED/net/trojan-go
-$PACKAGES_FEED/net/v2raya
-$PACKAGES_FEED/net/xray-core
 "
 
-backup()
-{
+backup() {
 # base
 if [ ! -f $STAMP_PACKAGE ]; then
   for PATCH in $PATCH_PACKAGE; do patch -p1 -b -d . < "$PATCH"; done
@@ -63,18 +61,28 @@ if [ ! -f $STAMP_PACKAGES ]; then
 fi
 }
 
-check()
-{
+check() {
 # base
-for PATCH in $PATCH_PACKAGE; do patch -p1 --dry-run -d . < "$PATCH"; done
+if [ ! -f $STAMP_PACKAGE ]; then
+  for PATCH in $PATCH_PACKAGE; do patch -p1 --dry-run -d . < "$PATCH"; done
+else
+  echo "base: Reversed (or previously applied) patch detected!"
+fi
 # oldports feed
-patch -p1 --dry-run -d $OLDPORTS_FEED < $PATCH_OLDPORTS
+if [ ! -f $STAMP_OLDPORTS ]; then
+  patch -p1 --dry-run -d $OLDPORTS_FEED < $PATCH_OLDPORTS
+else
+  echo "oldports: Reversed (or previously applied) patch detected!"
+fi
 # packages feed
-for PATCH in $PATCH_PACKAGES; do patch -p1 --dry-run -d $PACKAGES_FEED < "$PATCH"; done
+if [ ! -f $STAMP_PACKAGES ]; then
+  for PATCH in $PATCH_PACKAGES; do patch -p1 --dry-run -d $PACKAGES_FEED < "$PATCH"; done
+else
+  echo "packages: Reversed (or previously applied) patch detected!"
+fi
 }
 
-recovery()
-{
+recovery() {
 # base
 if [ -f $STAMP_PACKAGE ]; then
   for PATCH in $PATCH_PACKAGE; do patch -p1 -R -d . < "$PATCH"; done
